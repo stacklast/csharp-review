94a75cb6548f32e14ad96ff25f61555a39f33647
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-01-25 14:54:43 -0500

    Add Classes And Objects

---
+++ POO/ClassesAndObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sale sale1 = new Sale(100, DateTime.Now);
+            Sale sale2 = new Sale(100, DateTime.Now);
+            sale1.Show();
+            Console.WriteLine(sale1.GetInfo());
+            Console.WriteLine(sale2.GetInfo());
+        }
+    }
+
+    class Sale
+    {
+        int total;
+        DateTime date;
+
+        public Sale(int total, DateTime date)
+        {
+            this.total = total;
+            this.date = date;
+        }
+
+        public string GetInfo()
+        {
+            return total + " " + date.ToLongDateString();
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Hola Soy Una Venta");
+        }
+    }
+}
---
+++ POO/ClassesAndObjects/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClassesAndObjects")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassesAndObjects")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d528d564-26ce-484e-b1fd-aa63ff27a494")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
