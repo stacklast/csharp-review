1faaa7374d94bf686e23a5555587e10b21aedf72
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-01-25 15:06:00 -0500

    Add Properties

---
+++ POO/Properties/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sale mySale = new Sale(100, DateTime.Now);
+            mySale.Total = 120;
+            Console.WriteLine(mySale.Total);
+        }
+    }
+
+    class Sale
+    {
+        private int total;
+        DateTime date;
+
+        public string Date
+        {
+            get
+            {
+                return date.ToLongDateString();
+            }
+        }
+
+        public int Total
+        {
+            get
+            {
+                return total;
+            }
+            set
+            {
+                if (value < 0)
+                    value = 0;
+                total = value;
+            }
+        }
+
+        public Sale(int total, DateTime date)
+        {
+            this.total = total;
+            this.date = date;
+        }
+
+    }
+}
